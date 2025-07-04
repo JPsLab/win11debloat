e3ae94db9f64b8b4302cdb0de9dff28cee2b2f15
JP <110086239+JPsLab@users.noreply.github.com>
2025-07-04 08:33:29 +0200

    Update win11debloat.ps1

--- win11debloat.ps1
+++ win11debloat.ps1
@@ -19,5 +19,5 @@ $appNames = @(
 
 # Entfernen der Apps
 foreach ($app in $appNames) {
-    Get-AppxPackage "$app*" | Remove-AppxPackage -ErrorAction SilentlyContinue
+    Get-AppxPackage "$app*" -Scope CurrentUser | Remove-AppxPackage -ErrorAction SilentlyContinue
 }
